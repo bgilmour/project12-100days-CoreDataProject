d77adf787ba5129306a30393deb6200ba6d1325c
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-23 22:45:27 +0100

    One-to-many relationships with Core Data, SwiftUI, and @FetchRequest

---
+++ CoreDataProject/DataModel/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Bruce Gilmour on 2021-07-23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CoreDataProject/DataModel/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Bruce Gilmour on 2021-07-23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
